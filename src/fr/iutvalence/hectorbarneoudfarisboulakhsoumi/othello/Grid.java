ecec21867509f070fca141f57f03dde24768611e
Hector Barneoud <hector.barneoud@iut-valence.fr>
2015-05-07 10:56:18 +0200

    ...

--- src/fr/iutvalence/hectorbarneoudfarisboulakhsoumi/othello/Grid.java
+++ src/fr/iutvalence/hectorbarneoudfarisboulakhsoumi/othello/Grid.java
@@ -6,72 +6,33 @@ public class Grid {
 	private static final int SIDE_SIZE = 8;
 	/*  Set case table as attribut to the Grid. The grids could be composed of black or white pawn or nothing */
 	private final Case[][] cases;
-	
+	public int CASE_AVAILABLE_NUMBER;
 	/* This is the constructor for the Grid. */
 	public Grid()
 	{
+		CASE_AVAILABLE_NUMBER = 64;
 		
 		cases = new Case[SIDE_SIZE][SIDE_SIZE];
 		initGrid();
-		installPawn();
 	}
 	
-	private void installPawn(){
-		this.installPawn(new Position(3,4),new Pawn(Couleur.BLACK));
-		this.installPawn(new Position(4,3),new Pawn(Couleur.BLACK));
-		this.installPawn(new Position(3,3),new Pawn(Couleur.WHITE));
-		this.installPawn(new Position(4,4),new Pawn(Couleur.WHITE));
-		
-	}
-	
-	private void installPawn(Position position, Pawn pawn)
-	{
-		this.getCase(position).putPawn(pawn);
-	}
-	
-	private Case getCase(Position position)
-	{
-		return this.cases[position.getLineNumber()][position.getRowNumber()];
-	}
-	
-	/*public AvailableCasesVerification() throws NoCasesAvailable; // if no available case
-	{
-		if(CASE_AVAILABLE_NUMBER == 0) throw new NoCasesAvailable();
-		
-	}*/
-	
-	private void putPawn(Position position, Pawn pawn){
+	private void putPawn(Position position, Pawn pawn) throws NoCasesAvailable{
 		// TODO Verifier case disponible -> Exception 1
-		if(CASE_AVAILABLE_NUMBER == 0)
-			try {
-				throw new NoCasesAvailable();
-			} catch (NoCasesAvailable e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		if(CASE_AVAILABLE_NUMBER == 0) throw new NoCasesAvailable();
 			
-	
-			
-		// TODO Verifier les rÃ©gles de pose -> Exception 2
+		// TODO Verifier les régles de pose -> Exception 2
 		
 		// TODO Poser le pion.
-		// TODO RÃ©percuter le coup.
+		// TODO Répercuter le coup.
 			
-	
-	public String toString()
-	{
-		String gridLines = "-------------------------------------------------------\n";
-
-		for (int numeroDeLigne = 0; numeroDeLigne < SIDE_SIZE; numeroDeLigne++)
-		{
-			for (int numeroDeColonne = 0; numeroDeColonne < SIDE_SIZE; numeroDeColonne++)
-				gridLines += this.getCase(new Position(numeroDeLigne, numeroDeColonne));
-			gridLines += "\n-------------------------------------------------------\n";
-		}
-
-		return gridLines;
+		
 	}
 	
+	@Override
+	public String toString() {
+		// TODO
+		return super.toString();
+	}
 	
 	private void initGrid(){
 		for(int i = 0; i < SIDE_SIZE; i++) {
