de824792c3607a95faffe2e6eb605a48a4021c74
barneoudh <hector.barneoud@iut-valence.fr>
2015-05-20 23:18:12 +0200

    Add InvalidPositionException

---
+++ src/fr/iutvalence/hectorbarneoudfarisboulakhsoumi/othello/InvalidPositionException.java
@@ -0,0 +1,8 @@
+package fr.iutvalence.hectorbarneoudfarisboulakhsoumi.othello;
+/**
+ * Expection Invalid Position
+ */
+public class InvalidPositionException extends Exception {
+ 	private static final long serialVersionUID = 1L;
+
+}
